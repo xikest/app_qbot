ce82757929433cd24574705eadfa9ce2259a0542
xikest <xikest12@gmail.com>
2023-09-28 12:32:51 +0900

    Update src_macro.py

--- bots/src_generator/src_macro.py
+++ bots/src_generator/src_macro.py
@@ -15,10 +15,10 @@ class SrcMacro:
       class ShillerRatio:    
             @staticmethod
             def plot():
-                  yield Context(content = [ShillerRatio().plot("SHILLER_PE_RATIO_MONTH"),
-                                          ShillerRatio().plot("SP500_DIV_YIELD_MONTH"),
-                                          ShillerRatio().plot("SP500_EARNINGS_YIELD_MONTH"),
-                                          ShillerRatio().plot("SP500_REAL_PRICE_MONTH"),
+                  yield Context(content = [ShillerRatio().plot("SP500_DIV_YIELD_MONTH"),
+                                          # ShillerRatio().plot("SP500_EARNINGS_YIELD_MONTH"),
+                                          # ShillerRatio().plot("SP500_EARNINGS_YIELD_MONTH"),
+                                          # ShillerRatio().plot("SP500_REAL_PRICE_MONTH"),
                                           ShillerRatio().plot("SP500_PE_RATIO_MONTH")], dtype='img')
 
 
