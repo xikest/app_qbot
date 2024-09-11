82ded3f4c87435563be6945bee86ae368dae22e6
xik <xikest12@gmail..com>
2024-09-12 00:34:47 +0900

    .

--- bot/src_data/_tools.py
+++ bot/src_data/_tools.py
@@ -459,7 +459,7 @@ def _add_stock_sheet(fig: go.Figure, ds: pd.Series) -> go.Figure:
         max_dividends = df['Dividends'].max()
         # 정규화된 Dividends 값 계산
         if max_dividends == min_dividends:
-            size = 0.5
+            size = pd.Series(data=[0.5] *len(df), index=df.index)
             
         else:
             size = ((df['Dividends'] - min_dividends) / (max_dividends - min_dividends)) 
