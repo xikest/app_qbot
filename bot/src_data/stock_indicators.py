b0209825576cb4324e98f558f2b495ab44f2ad75
xikest <94699546+xikest@users.noreply.github.com>
2024-09-12 18:31:14 +0900

    Update stock_indicators.py

--- bot/src_data/stock_indicators.py
+++ bot/src_data/stock_indicators.py
@@ -20,8 +20,7 @@ class StockIndicators(Indicators):
         ds.name = key
 
         #if getattr(self,'to_pctchange_cum', False):
-        to_pctchange_cum = kwargs.get('to_pctchange_cum')
-        st.write(to_pctchange_cum)   
+        to_pctchange_cum = kwargs.get('to_pctchange_cum')   
         if to_pctchange_cum:           
             ds=ds.pct_change().cumsum()               
         return ds
