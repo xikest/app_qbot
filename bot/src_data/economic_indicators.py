8ed750870d84f84bd951a1f2f52977a381c2b159
xikest <94699546+xikest@users.noreply.github.com>
2024-09-13 11:27:58 +0900

    Update economic_indicators.py

--- bot/src_data/economic_indicators.py
+++ bot/src_data/economic_indicators.py
@@ -1,5 +1,5 @@
 from pandas import Series
-from fredapi import Fred
+import FinanceDataReader as fdr
 from ._abstract_indicators import Indicators
 from ._tools import validate_date, index_to_datetime, Plot
 import streamlit as st
@@ -8,14 +8,12 @@ class EconomicIndicators(Indicators):
 
     def __init__(self, api_key: str = None):
         super().__init__(indicators_file="economic.json")
-        if api_key is None:
-            self.api_key = "1afc3162f75a055edf1d1a95529096cf"
 
     @Plot(mode_binary=False, plot_type='line')
     @index_to_datetime
     @validate_date
     def _request(self, key: str = 'CPIAUCSL', name: str = None,
                  start: str = None, end: str = None, *args, **kwargs) -> Series:
-        ds = Fred(api_key=self.api_key).get_series(key, observation_start=start, observation_end=end)
+        ds = fdr.DataReader(f'FRED:{key}', start=start, end=end).iloc[:,0]
         ds.name = name
         return ds
